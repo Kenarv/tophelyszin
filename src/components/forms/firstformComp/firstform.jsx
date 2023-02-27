2604046f9ab8e2a16152083bab6c44d204b14245
Vranek Bálint <balintvranek@gmail.com>
2023-02-27 18:45:11 +0100

    kis szepités

--- src/components/forms/firstformComp/firstform.jsx
+++ src/components/forms/firstformComp/firstform.jsx
@@ -1,7 +1,7 @@
 
 const FirstForm = () => {
     return (
-        <div className="animate-form-animation flex flex-col gap-2 w-[30rem] mx-auto form-animation p-20 bg-sky-300/30 rounded-xl">
+        <div className="animate-form-animation flex flex-col gap-2 w-[30rem]  mx-auto form-animation p-20 bg-sky-300/30 rounded-xl shadow-lg">
             <div className="flex flex-col">
                 <label className="font-bold" htmlFor="name">
                     felhasználónév:
@@ -26,7 +26,7 @@ const FirstForm = () => {
             </div>
             <div className="flex flex-row gap-20">
                 <button
-                    className="transition duration-300 ease-in-out text-xl w-35 px-5 py-1 text-white rounded-3xl bg-sky-400/30 hover:bg-sky-300/50"
+                    className="transition duration-300 ease-in-out text-xl w-35 px-5 py-1 text-white rounded-3xl bg-sky-400/30 hover:bg-sky-300/50 shadow-lg"
                     onClick={() => ''}
                 >
                     <p className='text-slate-700 font-semibold text-base'>Belépés</p> 
--- src/components/forms/firstformComp/showSwapperButton.jsx
+++ src/components/forms/firstformComp/showSwapperButton.jsx
@@ -3,7 +3,7 @@ const ShowSwapperButton = props => {
 
     return (
         <button
-            className='transition duration-300 ease-in-out text-xl w-35 px-6 py-2 ml-15 text-white rounded-3xl bg-sky-300/30 hover:bg-sky-400/50'
+            className='transition duration-300 ease-in-out text-xl w-35 px-6 py-2 ml-15 text-white rounded-3xl bg-sky-300/30 hover:bg-sky-400/50 shadow-lg'
 
             onClick={!show ? () => setShow(true) : () => setShow(false)}
         >
