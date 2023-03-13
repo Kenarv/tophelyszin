310967b1d130c61bb21fa83d5a96d606b1aa8cbe
Vranek Bálint <115113230+Kenarv@users.noreply.github.com>
2023-03-13 12:19:58 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
+            screens: {
+                'tablet': '640px',
+                // => @media (min-width: 640px) { ... }
+          
+                'laptop': '1024px',
+                // => @media (min-width: 1024px) { ... }
+          
+                'desktop': '1280px',
+                // => @media (min-width: 1280px) { ... }
+            },
+
             backgroundImage: {
                 wedding: "url('./media/background.jpg')",
             },
