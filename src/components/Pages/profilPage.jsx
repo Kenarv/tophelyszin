d2bf3c7687a732f3aed2ebc7645249ae256429b2
Vranek Bálint <balintvranek@gmail.com>
2023-03-22 12:19:35 +0100

    profil page formázás

--- src/components/Pages/profilPage.jsx
+++ src/components/Pages/profilPage.jsx
@@ -1,21 +1,33 @@
 import UploadShowButton from './uploadShowbutton';
-
+import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
 const ProfilPage = ({ uploadShowButtonObj }) => {
     return (
         <>
             <div className="animate-form-animation flex flex-col gap-2 w-[360px] desktop:w-[1200px] mx-auto form-animation p-20 bg-sky-300/30 rounded-xl shadow-lg">
                 <div className="grid grid-rows-3">
-                    <div>11</div>
-                    <div>22</div>
-                    <div className="items-center">
+                    <div className="w-2/5 ">
+                        <h1 className="text-2xl font-bold">Helyszín neve</h1>
+                        <div className="text-base columns-1 desktop:columns-2 gap-2">
+                            <div className="flex align-center ">
+                                <AiOutlineMail className="text"/>
+                                <p>Email@Email</p>
+                            </div>
+                            <div className="flex">
+                                <AiOutlinePhone/>
+                                <p>+36301234567</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border-solid border-sky-500 rounded-lg border-2 ">
+                        "itt lesznek a helyszin profilon található feltöltések"
+                    </div>
+                    <div className=" grid justify-items-center">
                         <UploadShowButton
                             uploadShowButtonObj={uploadShowButtonObj}
                         />
                     </div>
                 </div>
             </div>
-
-            {document.get}
         </>
     );
 };
