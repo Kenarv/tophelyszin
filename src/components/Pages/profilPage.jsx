56e95e87c37f3de32eb85c415362dc7b600ff5a7
Vranek Bálint <balintvranek@gmail.com>
2023-03-27 17:47:00 +0200

    törlés gomb hozzáadás

--- src/components/Pages/profilPage.jsx
+++ src/components/Pages/profilPage.jsx
@@ -6,8 +6,8 @@ const ProfilPage = ({ uploadShowButtonObj }) => {
     return (
         <>
             <div className="animate-form-animation flex flex-col gap-2 w-[360px] laptop:w-[680px] desktop:w-[1200px] mx-auto form-animation p-2 bg-sky-500/30 rounded-xl shadow-lg">
-                <div className="grid grid-rows-3 p-10">
-                    <div className="w-2/5 h-[100px]">
+                <div className=" p-10 grid-flow-row auto-rows-max">
+                    <div className="w-2/5 mb-5">
                         <h1 className="text-3xl font-bold">Gipsz Jakab</h1>
                         <div className="text-base columns-1 desktop:columns-2 gap-2 py-2">
                             <div className="flex items-center gap-2">
@@ -21,7 +21,7 @@ const ProfilPage = ({ uploadShowButtonObj }) => {
                         </div>
                     </div>
                     <div className="border-solid border-sky-500 rounded-lg border-2">
-                        <div className="grid grid-cols-1 gap-5 p-5 justify-center w-full">
+                        <div className="grid grid-cols-1 gap-5 p-5 w-full">
                             {venues.map(element => {
                                 return (
                                     <div
@@ -55,12 +55,24 @@ const ProfilPage = ({ uploadShowButtonObj }) => {
                                                 {element.hazszam}
                                             </p>
                                         </div>
+                                        <div className="grid col-span-3 justify-items-end">
+                                            <div>
+                                                <button
+                                                    className={`transition duration-300 ease-in-out bg-red-500/70 text-xl h-12 w-35 px-6 py-2 ml-15 hover:bg-red-200/70 text-white rounded-3xl shadow-lg`}
+                                                    onClick={() => {}}
+                                                >
+                                                    <p className="text-slate-700 font-semibold text-lg">
+                                                        törlés
+                                                    </p>
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
                                 );
                             })}
                         </div>
                     </div>
-                    <div className=" grid justify-items-center">
+                    <div className=" grid justify-items-center mt-3 mb-24">
                         <UploadShowButton
                             uploadShowButtonObj={uploadShowButtonObj}
                         />
