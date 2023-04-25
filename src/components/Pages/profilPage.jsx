44ccf1038ddee81a7d343ef01d15475376f6670c
Vranek Bálint <115113230+Kenarv@users.noreply.github.com>
2023-04-25 13:47:05 +0200

    log tries

--- src/components/Pages/profilPage.jsx
+++ src/components/Pages/profilPage.jsx
@@ -13,7 +13,7 @@ const ProfilPage = ({
     const { setShowPwChange } = pwChangeShowObj;
     const { setLogged } = loggedButtonObj;
     console.log(clicked);
-
+    console.log(clickedID);
 
 
 
@@ -65,19 +65,19 @@ const ProfilPage = ({
                             {venues.map(element => {
                                 const nameLength = String(element.name).length;
 
-                                console.log(clickedID);
-                                console.log(clicked);
-
+                               
                                 return (
                                     <div
                                         key={element.id}
                                         className="grid grid-flow-row auto-rows-auto grid-cols-1 desktop:grid-cols-3 gap-2 bg-sky-200/40 w-full p-5 rounded-xl border-2 border-sky-400/80 hover:bg-sky-500/20 duration-300"
                                         onClick={() => 
-                                            !clicked
+                                            (!clicked
                                             ? setClicked(true)
                                             : setClicked(false) 
-
-                                            //console.log(element.id)
+                                            )
+                                            &&
+                                            setClickedID(element.id)
+                                            
                                         }
                                     >
                                         <div className="row-span-3">
