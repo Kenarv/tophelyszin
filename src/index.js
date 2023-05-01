0588445b6003c7fa9016907556f7de46a3801b3e
Vranek Bálint <balintvranek@gmail.com>
2023-05-01 17:45:09 +0200

    Update package-lock.json

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+//import {DevSupport} from "@react-buddy/ide-toolbox";
+//import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
+       
             <App/>
-        </DevSupport>
+        
     </React.StrictMode>
 );
 
